9d5ec6a07934f419140d945ab4b4a58d50910c4f
Dave Harms <daveharmswebdev@gmail.com>
2016-09-03 12:35:52 -0500

    made html and js, added controllers in controller/index need to add routs to main.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,3 +4,7 @@ const app = require('angular').module('profile')
 
 app.controller('splashController', require('./splash'))
 app.controller('navController', require('./navController'))
+app.controller('blogController', require('./blogController'))
+app.controller('contactController', require('./contactController'))
+app.controller('aboutController', require('./aboutController'))
+app.controller('projectsController', require('./projectsController'))
